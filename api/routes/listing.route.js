1b95ec4d9003bd6f1897433e675e704d4adc1db4
Sachith Avintha <153298821+Sachith45@users.noreply.github.com>
2024-05-28 22:43:05 +0530

    create listing API route

---
+++ api/routes/listing.route.js
@@ -0,0 +1,9 @@
+import { verifyTokrn } from '../utils/verifyUset.js';
+import { create } from "../controller/listing.controllet.js";
+import express from 'express'
+
+const router = express.Router();
+
+router.post('/create',verifyTokrn,create);
+
+export default router;
